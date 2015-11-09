547d6c1c5ee46209350e30f2e8fa5215c4a339ce
Sachin <schnmudgal@gmail.com>
2015-11-09 18:14:36 +0530

    Mailer config on prod testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+# Default url
+  Rails.application.routes.default_url_options = { host: 'http://wachasay.herokuapp.com' }
+  config.action_controller.default_url_options = { host: 'http://wachasay.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://wachasay.herokuapp.com' }
+
+  # Mailer
+  config.action_mailer.default_url_options = { :host => '' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "wachasayofficial@gmail.com",
+    :password             => "sactar2914",
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
 end
