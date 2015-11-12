8a7535f99f33190fceab6da432a8ce9c1cb035b6
Sachin <schnmudgal@gmail.com>
2015-11-13 01:28:02 +0530

    Unwanted fb logout button removed

--- app/assets/javascripts/tags_manager.js
+++ app/assets/javascripts/tags_manager.js
@@ -1,13 +1,13 @@
-function TagsManager (argument) {
-  this.$body = $('body');
-  this.taggableFields = document.querySelectorAll('[taggable_field="true"]');
-  this.tagStartChar = '#';
-};
+// function TagsManager (argument) {
+//   this.$body = $('body');
+//   this.taggableFields = document.querySelectorAll('[taggable_field="true"]');
+//   this.tagStartChar = '#';
+// };
 
-TagsManager.prototype.init = function() {
-  this.bindTagsDisplay();
-};
+// TagsManager.prototype.init = function() {
+//   this.bindTagsDisplay();
+// };
 
-TagsManager.prototype.bindTagsDisplay = function() {
-  this.
-};
+// TagsManager.prototype.bindTagsDisplay = function() {
+//   this.taggableFields;
+// };
